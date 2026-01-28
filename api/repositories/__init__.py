9a0660d0a0069cd80e7fc05d5a6f836d5772d5b1
preszzz <zh.preston@gmail.com>
2026-01-28 14:33:07 +0800

    fix: remove sending assets later on related prompt

--- api/repositories/__init__.py
+++ api/repositories/__init__.py
@@ -3,4 +3,8 @@ from .tenant_repo import TenantRepository
 from .thread_repo import ThreadRepository
 
 
-__all__ = ["AssistantRepository", "TenantRepository", "ThreadRepository"]
+__all__ = [
+    "AssistantRepository",
+    "TenantRepository",
+    "ThreadRepository"
+]
